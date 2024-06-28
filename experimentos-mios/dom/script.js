e4ed08d7aa6a75bbed06734ea3f8afd655dc1005
ezechavezz <ezequielmartinchavez@gmail.com>
2024-06-28 16:03:55 -0300

    actualizacion de experimentos

--- experimentos-mios/dom/script.js
+++ experimentos-mios/dom/script.js
@@ -1,3 +1,17 @@
 let divNode = document.querySelector('.textito');
 
+const funcion = (parameteros) => {
+    //cuerpo de la funcion
+}
+//MAP Recibe funcion transformadora
+//4 elementos
+let numeros = [1,2,3,4,5];
+let cuadradoNumeros = numeros.map( x => x*2);
+// 2,4,6,8,10
+console.log(cuadradoNumeros);
+
+const boton = document.getElementById('boton');
+boton.addEventListener('click', function() {
+  alert('¡Hola!');
+});
 

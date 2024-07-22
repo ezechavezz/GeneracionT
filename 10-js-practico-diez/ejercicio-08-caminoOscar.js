eb4f5d54f0c6236811f1c410dd07b3c2be6e5d01
ezechavezz <ezequielmartinchavez@gmail.com>
2024-07-22 16:17:49 -0300

    terminar el 8

---
+++ 10-js-practico-diez/ejercicio-08-caminoOscar.js
@@ -0,0 +1,6 @@
+let greeting = "Hello and welcome"
+greeting[0] //H
+let words = greeting.split('')
+
+let actoresVocales = []
+let actoresPrincipales = ["Tom Hanks", "Meryl Streep", "Leonardo DiCaprio", "Jennifer Lawrence", "Denzel Washington"]

21887da581af5f569d9ab22708c46b4cc519cf45
ezechavezz <ezequielmartinchavez@gmail.com>
2024-07-22 14:08:54 -0300

    Ejercicio 2

---
+++ 10-js-practico-diez/ejercicio-02.js
@@ -0,0 +1,13 @@
+let capitales = {
+    Argentina: 'Buenos Aires',
+    Uruguay: 'Montevideo',
+    Brasil: 'Brasilia',
+}
+
+let lugar = 'Uruguay'
+
+console.log(capitales['lugar']);
+
+console.log(capitales.lugar);
+
+console.log(capitales[lugar]);

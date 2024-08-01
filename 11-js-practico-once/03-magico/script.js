91beaf5d88117f3959829e7c7ca963c3804477a1
ezechavezz <ezequielmartinchavez@gmail.com>
2024-08-01 19:02:55 -0300

    magico empezado

---
+++ 11-js-practico-once/03-magico/script.js
@@ -0,0 +1,6 @@
+const magiaImg = document.getElementById("magia");
+const loboSolitario = magiaImg.querySelector('img');
+
+magiaImg.addEventListener('mouseenter', function() {
+    image.style.display = 'none';
+});
---
+++ React/a.js
@@ -0,0 +1,8 @@
+// archivo main.js
+import greet, { pi, add } from './math-and-greet.js';
+
+console.log(greet('World')); // Hello, World!
+console.log(pi); // 3.14159
+console.log(add(2, 3)); // 5
+
+d
